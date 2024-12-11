067b3e64483a16091c2f87babeed3498e921b8e6
InfinumN <myasnna@gmail.com>
2024-12-11 12:38:11 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,14 @@
+#note_manager01
+
+username = 'Наталья'
+print('Имя пользователя: ', username)
+title = 'Этап 1: Задание 1'
+print('Заголовок: ', title)
+content = 'Задание выполнено успешно. Или нет?..'
+print('Заметка: ', content)
+status = 'хз'
+print('Статус заметки: ', status)
+created_date = '11-12-2024'
+print('Дата создания день-месяц-год: ',created_date)
+issue_date = '11-12-2024'
+print('Дедлайн заметки день-месяц-год: ',issue_date)
