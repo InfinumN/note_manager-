07ea6506bef6d3cf82d3219de6c65f9559196de4
InfinumN <myasnna@gmail.com>
2024-12-21 16:57:31 +0300

    Add files via upload

---
+++ update_status.py
@@ -0,0 +1,19 @@
+note = {'status': 'отложено'}
+available_status = ['выполнено', 'в процессе', 'отложено'] #Возможные статусы
+
+status_temp = 3 #будущий индекс списка доступных статусов
+
+print('Обновление статуса заметки.')
+while status_temp not in [0, 1, 2]:
+    status_temp = input('Доступные варианты: 0 - выполнено, 1 - в процессе, 2 - отложено. Ввод: ')
+    #Пытаемся обновить статус заметки
+    try:
+        status_temp = int(status_temp)
+        note['status'] = available_status[status_temp]
+        print('Статус заметки успешно обновлён:', available_status[status_temp])
+    except ValueError:
+        print('Ошибка, введено не число! Повторите попытку.')
+    except IndexError:
+        print('Ошибка, введено неизвестное число, выберите пожалуйста из списка.')
+
+print('Программа успешно завершила работу.')
